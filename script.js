be8be99acdc566878c18872cd3ca70d84b5ca6f6
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-07-22 13:31:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// //your JS code here. If required.
-// // Function that returns a promise which resolves with an array of numbers after 3 seconds
-// function getNumbers() {
-//   return new Promise(resolve => {
-//     setTimeout(() => {
-//       resolve([1, 2, 3, 4]);
-//     }, 3000);
-//   });
-// }
-
-// // Function to filter out odd numbers
-// function filterOddNumbers(numbers) {
-//   return new Promise(resolve => {
-//     setTimeout(() => {
-//       const evenNumbers = numbers.filter(num => num % 2 === 0);
-//       resolve(evenNumbers);
-//     }, 1000);
-//   });
-// }
 // Function that returns a promise which resolves with an array of numbers after 3 seconds
 function getNumbers() {
   return new Promise(resolve => {
@@ -67,31 +48,3 @@ window.onload = function () {
     .then(evenNumbers => multiplyEvenNumbers(evenNumbers))
     .catch(error => console.error('Error:', error));
 };
-
-// // Function to multiply even numbers by 2
-// function multiplyEvenNumbers(numbers) {
-//   return new Promise(resolve => {
-//     setTimeout(() => {
-//       const multipliedNumbers = numbers.map(num => num * 2);
-//       resolve(multipliedNumbers);
-//     }, 2000);
-//   });
-// }
-
-// // Update the text content of the div with the id 'output'
-// function updateOutput(content) {
-//   const outputDiv = document.querySelector('#output');
-//   outputDiv.textContent = content.join(', ');
-// }
-
-// // Execute the promise chain
-// getNumbers()
-//   .then(numbers => filterOddNumbers(numbers))
-//   .then(evenNumbers => {
-//     updateOutput(evenNumbers);
-//     return multiplyEvenNumbers(evenNumbers);
-//   })
-//   .then(multipliedNumbers => {
-//     updateOutput(multipliedNumbers);
-//   })
-//   .catch(error => console.error('Error:', error));
